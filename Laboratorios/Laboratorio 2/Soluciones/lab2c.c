54902c5bd3350bbbfb3cfc0a25c0df5dd8261941
juanesgl <juan.sgarcia@mail.escuelaing.edu.co>
2025-08-28 22:20:05 -0500

    last problem of lab 2

--- Laboratorios/Laboratorio 2/Soluciones/lab2c.c
+++ Laboratorios/Laboratorio 2/Soluciones/lab2c.c
@@ -1,12 +1,61 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int mcd(int a, int b){
 
+    while (b != 0){
 
+        int temp = b;
+        b = a % b; 
+        a = temp;
+    }    
+    return a;
+}
 
-int main(){
+int main() {
+    int tc, i;
+    long numerator, denominator, n;
+    long common_divisor;
 
+    scanf("%d", &tc);
 
+    for (i = 0; i < tc; i++) {
+        scanf("%ld", &numerator);
+        scanf("%ld", &denominator);
+     
+        if (numerator == 0) {
+            printf("0\n");
+            printf("0\n");
+            continue;
+        }
 
+        while (numerator != 1) {
+            
+            if (numerator == 0) break;
+            
+            n = denominator / numerator;
+            if (denominator % numerator != 0) {
+                n++;
+            }
 
+            printf("%ld\n", n);
+            
+            numerator = numerator * n - denominator;
+            denominator = denominator * n;
+          
+            if (numerator != 0) {
+                common_divisor = mcd(numerator, denominator);
+                numerator /= common_divisor;
+                denominator /= common_divisor;
+            }
+        }
+       
+        if (numerator == 1) {
+            printf("%ld\n", denominator);
+        }
+        
+        printf("0\n");
+    }
+
+    return 0;
 }

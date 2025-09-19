fb3d0f61a3d78a2660074e3149ab2f460295496d
juanesgl <juan.sgarcia@mail.escuelaing.edu.co>
2025-09-19 08:52:38 -0500

    ...

--- Proyectos/Proyecto 1/division.c
+++ Proyectos/Proyecto 1/division.c
@@ -1,113 +1,86 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-#define MAX 350001
 
-
-int dividend[MAX], divisor[MAX], temp[MAX], quotient[MAX];
-int lenA, lenB, lenTemp, qlen;
-
-
-int compare(int arr1[], int len1, int arr2[], int len2) {
-    int i;
-    if (len1 > len2) return 1;
-    if (len1 < len2) return -1;
-    for (i = 0; i < len1; i++) {
-        if (arr1[i] > arr2[i]) return 1;
-        if (arr1[i] < arr2[i]) return -1;
-    }
-    return 0;
+void stripLeadingZeros(char *number) {
+    int length = strlen(number);
+    int i = 0;
+    while (i < length-1 && number[i] == '0') i++;
+    if (i > 0) memmove(number, number+i, length-i+1);
 }
 
 
-void subtract(int arr1[], int *len1, int arr2[], int len2) {
-    int i, j, borrow = 0;
-    for (i = 0; i < *len1; i++) {
-        int a = arr1[*len1 - 1 - i];
-        int b = (i < len2) ? arr2[len2 - 1 - i] : 0;
-        int val = a - b - borrow;
-        if (val < 0) {
-            val += 10;
+int compareNumbers(char *num1, char *num2) {
+    stripLeadingZeros(num1);
+    stripLeadingZeros(num2);
+    {
+        int len1 = strlen(num1), len2 = strlen(num2);
+        if (len1 != len2) return len1 - len2;
+        return strcmp(num1, num2);
+    }
+}
+
+
+void subtract(char *minuend, char *subtrahend) {
+    int lenMinuend = strlen(minuend), lenSubtrahend = strlen(subtrahend);
+    int borrow = 0;
+    int i;
+    for (i = 0; i < lenMinuend; i++) {
+        int digitMinuend = minuend[lenMinuend-1-i] - '0';
+        int digitSubtrahend = (i < lenSubtrahend) ? subtrahend[lenSubtrahend-1-i] - '0' : 0;
+        int difference = digitMinuend - digitSubtrahend - borrow;
+        if (difference < 0) {
+            difference += 10;
             borrow = 1;
         } else {
             borrow = 0;
         }
-        arr1[*len1 - 1 - i] = val;
+        minuend[lenMinuend-1-i] = difference + '0';
     }
-   
-    while (*len1 > 1 && arr1[0] == 0) {
-        for (j = 0; j < *len1 - 1; j++) {
-            arr1[j] = arr1[j + 1];
-        }
-        (*len1)--;
-    }
+    stripLeadingZeros(minuend);
 }
 
 
-void divide(char A[], char B[]) {
-    int i, j;
+void divide(char *dividend, char *divisor, char *quotient, char *remainder) {
+    int lenDividend = strlen(dividend);
+    char currentSubstring[350005];
+    currentSubstring[0] = '\0';
+    int quotientIndex = 0;
+    int i;
 
-    lenA = 0; lenB = 0; lenTemp = 0; qlen = 0;
+    for (i = 0; i < lenDividend; i++) {
+        int length = strlen(currentSubstring);
+        currentSubstring[length] = dividend[i];
+        currentSubstring[length+1] = '\0';
+        stripLeadingZeros(currentSubstring);
 
-    while (A[lenA] != '\0') {
-        dividend[lenA] = A[lenA] - '0';
-        lenA++;
-    }
-    while (B[lenB] != '\0') {
-        divisor[lenB] = B[lenB] - '0';
-        lenB++;
-    }
-
-    for (i = 0; i < lenA; i++) {
-        
-        temp[lenTemp] = dividend[i];
-        lenTemp++;
-
-        while (lenTemp > 1 && temp[0] == 0) {
-            for (j = 0; j < lenTemp - 1; j++) temp[j] = temp[j + 1];
-            lenTemp--;
+        {
+            int count = 0;
+            while (compareNumbers(currentSubstring, divisor) >= 0) {
+                subtract(currentSubstring, divisor);
+                count++;
+            }
+            quotient[quotientIndex++] = count + '0';
         }
-
-        
-        int count = 0;
-        while (compare(temp, lenTemp, divisor, lenB) >= 0) {
-            subtract(temp, &lenTemp, divisor, lenB);
-            count++;
-        }
-        quotient[qlen++] = count;
     }
-
-  
-    i = 0;
-    while (i < qlen - 1 && quotient[i] == 0) i++;
-
-  
-    for (; i < qlen; i++) {
-        printf("%d", quotient[i]);
-    }
-    printf("\n");
-
-   
-    if (lenTemp == 0) {
-        printf("0\n");
-    } else {
-        for (i = 0; i < lenTemp; i++) {
-            printf("%d", temp[i]);
-        }
-        printf("\n");
-    }
+    quotient[quotientIndex] = '\0';
+    stripLeadingZeros(quotient);
+    strcpy(remainder, currentSubstring);
+    if (strlen(remainder) == 0) strcpy(remainder, "0");
 }
 
 int main() {
-    int t;
-    char A[MAX], B[MAX];
-    if (scanf("%d", &t) != 1) return 1;
+    int numTests;
+    scanf("%d", &numTests);
+    while (numTests--) {
+        char dividend[350005], divisor[350005];
+        char quotient[350005], remainder[350005];
+        scanf("%s %s", dividend, divisor);
 
-    while (t--) {
-        scanf("%s", A);
-        scanf("%s", B);
-        divide(A, B);
+        divide(dividend, divisor, quotient, remainder);
+
+        printf("%s\n%s\n", quotient, remainder);
     }
-
     return 0;
 }
